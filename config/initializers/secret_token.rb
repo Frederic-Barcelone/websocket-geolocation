b2e140c6d47e1d723b4df32c0b1c5ca180c7e253
Takaaki Muta <takaaki.muta@optim.co.jp>
2012-01-29 22:21:20 +0900

    railsプロジェクトを初期コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsoketGeolocation::Application.config.secret_token = '495a107369db646382f335a39d766fd22abb85b81e10e1ea155b098fc372fdca323970d45302c047efc13be438e8ece9ab7ff718049adcb543a59d8980f0af4c'

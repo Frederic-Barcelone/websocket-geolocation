0d9a66f38c56e2a61588edfb92989463707e4e93
Takaaki Muta <takaaki.muta@optim.co.jp>
2012-02-09 23:31:59 +0900

    レイアウトを調整
    ログイン、ログアウトリンクを調整
    とりあえずデフォルトの画像を追加してみた。
    あとでレイアウト,cssを調整する

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ WebsoketGeolocation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'login#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

5a6d5517769632cc11dff7748157cd386ac4bde9
Takaaki Muta <takaaki.muta@gmail.com>
2012-02-25 22:24:39 +0900

    パース処理が2回走っていたので不要なパースを無効化した

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,7 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup('application/xml')] = lambda do |body|
+  return false
+end
--- script/mocks/mock_client.rb
+++ script/mocks/mock_client.rb
@@ -1,3 +1,3 @@
 require 'xmlrpc/client'
 server = XMLRPC::Client.new2("http://localhost:3000/api/xmlrpc")
-p server.call("authenticate", "takaaki0@hyper.ocn.ne.jp", "ISHPmb")
+p server.call("authenticate", "takaaki0@hyper.ocn.ne.jp", "ISHPmb", {"hoge" => 1, "piyo" => { "1" => true}})

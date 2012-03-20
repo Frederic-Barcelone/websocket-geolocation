de91a92a9c6295b9fb3b96c123674e57f1e51d8d
Takaaki Muta <takaaki.muta@gmail.com>
2012-03-20 19:00:40 +0900

    easy_xmlrpcのバージョンアップを行ったのでこちらも合わせる

--- app/controllers/xmlrpc_controller.rb
+++ app/controllers/xmlrpc_controller.rb
@@ -1,13 +1,16 @@
 class XmlrpcController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  xmlrpc
-  include XmlrpcErrors
+  xmlrpc_setup
+
+  xmlrpc_error :user_not_found_error, :code => 1, :message => "UserNotFoundError"
+  xmlrpc_error :device_imei_not_found_error, :code => 2, :message => "DeviceImeiNotFoundError"
+  xmlrpc_error :device_not_found_error, :code => 3, :message => "DeviceNotFoundError"
 
   def authenticate(email, certification_code, system)
     if user = User.find_by_email_and_certification_code(email,certification_code)
       imei = system.delete("imei")
-      raise DeviceImeiNotFoundError if imei.blank?
+      raise device_imei_not_found_error if imei.blank?
       android = user.androids.find_or_create_by_imei(imei)
       system.each_pair do |key, value|
         android.additional_device_informations.create(:key => key, :value => value)
@@ -15,7 +18,7 @@ class XmlrpcController < ApplicationController
 
       return android.uuid
     else
-      raise UserNotFoundError
+      raise user_not_found_error
     end
   end
 
@@ -27,7 +30,7 @@ class XmlrpcController < ApplicationController
       end
       location.save!
     else
-      raise DeviceNotFoundError
+      raise device_not_found_error
     end
     return_success
   end
